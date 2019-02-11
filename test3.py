e1da3a5339a923b2977c70bb0f3db225978c2415
MehrdadFAR <mh.farsadyar@gmail.com>
2019-02-11 17:51:34 +0100

    Update test3.py
    Issue with index of output file is solved. If block added to check the input arguments, allows better processign of extra arguments compared to previous implementation. Added if condition to check the name of the output file inorder to append to it ".csv" to make it proper cdv file.

--- test3.py
+++ test3.py
@@ -4,10 +4,30 @@ import os
 
 if __name__ == "__main__":
 
-    training = open(sys.argv[1])
-    test = open(sys.argv[2])
-    output = sys.argv[3]
+    arguments = sys.argv
+    
+    # This block checks if three arguments are provided, and assigns them to variables.
+    if len(arguments) < 4:
+        raise Exception("Provide three arguments: Address_Input Address_Test OutputName.csv")
+    elif len(arguments) == 4:
+        training = open(arguments[1]) # arguments[1] is the absolute path to the training input file
+        # test = open(arguments[2]) # arguments[2] is the absolute path to the test input file
+        outputName = arguments[3] # name of the output file
+    else:
+        pass # TO_DO: implementation of fetching extra variables.
+
+    # If output name does not end with .csv or .txt append to it .csv
+    if not (outputName.endswith(".csv") or outputName.endswith(".txt")):
+        outputName = outputName + ".csv"
+    
+    # Read the CSV file
     inData = pd.read_csv(training)
-    inData = inData.reindex( columns = inData.columns.tolist() + ["newColumn"])
-    inData.to_csv(output)
-    print(inData)
+    print(inData.head()) # TO_DO: REMOVE before dispatch
+
+    # Add extra column for naive predictor
+    inData['Naive_Predictor'] = 0 # TO_DO: "0" to be replaced with actual prediction
+    print(inData.head()) # TO_DO: REMOVE before dispatch
+
+    # Output the result a file
+    inData.to_csv(outputName, index=False)
+
