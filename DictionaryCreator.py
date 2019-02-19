7b8a8e5458c057cbcea948afc96d6662525d66a7
MehrdadFAR <mh.farsadyar@gmail.com>
2019-02-19 09:23:35 +0100

    Added dictionary creator.
    Added dictionary creator.

---
+++ DictionaryCreator.py
@@ -0,0 +1,43 @@
+# new version
+import pandas as pd
+
+class DictionaryCreator:
+        df_list = None  #????possibility to assign value at objet creation via constructor
+
+        minDict = {}
+        accDict = {}
+        remainDict = {} # combine with acc dic??
+        mainDict = {}
+
+        # createa new dictionary with key eventID and values: Event Time, Event Name Life Cycle THEN add it to mainDict to the list corrspondng to the caseName
+        def create_main_dictionary(self, df_list):
+            for df in df_list:
+                tempDF = pd.DataFrame(df)
+                for index, row in tempDF.iterrows():
+                    e_ID = row["eventID "]
+                    case_concept_name = str([row["case concept:name"]])
+
+                    tempDict = {e_ID: {"event concept:name": row["event concept:name"],
+                                                "event lifecycle:transition": row["event lifecycle:transition"],
+                                                "event time:timestamp": row["event time:timestamp"]
+                                       }
+                                }
+
+                    self.mainDict.setdefault(case_concept_name, []).append(tempDict)
+
+                    # create min dictionary as side effect to prevent looping twice.
+                    event_time_stamp = pd.to_datetime(tempDict[e_ID][ "event time:timestamp"], format='%d-%m-%Y %H:%M:%S.%f')
+
+                    min = self.minDict.setdefault(case_concept_name, float("inf"))
+
+                    if event_time_stamp < min:
+                        self.minDict[case_concept_name] = event_time_stamp
+
+            return self.mainDict
+
+        # @pre: create_main_dictionary must have been called once before or else an exception will be thrown.
+        def create_min_dictionary(self):
+            if self.minDict:
+                return self.minDict
+            else:
+                raise Exception("method create_main_dictionary was not called at least once to create the mindict as side effect")
--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ import os
 import numpy as np
 import csv
 from InputProcessor import InputProcessor
+from DictionaryCreator import DictionaryCreator
+
 
 if __name__ == "__main__":
 
@@ -14,15 +16,13 @@ if __name__ == "__main__":
     Input_Processor = InputProcessor()
     df_list = Input_Processor.read_file(trainingAddress, anEncoding)
 
+    #for c, v, v2 in zip(list(df_list[0].columns.values), list(df_list[0].loc[1]), list(df_list[0].loc[2])):
+        #print(c, " : ",  v, " | ", v2)
 
+    #df = pd.DataFrame(df_list[0])
+    #print(df.head())
 
-    print(list(df_list[0].columns.values))
-
-    print(list(df_list[0].loc[1]))
-
-
-    for c, v, v2 in zip(list(df_list[0].columns.values), list(df_list[0].loc[1]), list(df_list[0].loc[2])):
-        print(c, " : ",  v, " | ", v2)
-
-    df = pd.DataFrame(df_list[0])
-    print(df.head())
+   #--***--
+    training_DictionaryCreator = DictionaryCreator()
+    training_main_dictionary = training_DictionaryCreator.create_main_dictionary(df_list)
+    training_min_dictionary = training_DictionaryCreator.create_min_dictionary()
