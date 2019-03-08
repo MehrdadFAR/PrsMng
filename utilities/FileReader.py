432f7655d1cbf41c322c9be90702aaa9aeedd266
MehrdadFAR <mh.farsadyar@gmail.com>
2019-03-08 21:53:44 +0100

    The new Tool.
    The new Tool. Also added directories.

--- utilities/FileReader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-class FileReader:
-    anEncoding = None
-    def __init__(self, encoding):
-        self.anEncoding = encoding
-
-    def readFile(self, address):
-        return pd.read_csv(address, encoding=self.anEncoding, low_memory=False)
--- utilities/MemoryUsage.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-class MemoryUsage:
-    #in MegaBytes
-    def memory_usage_sum(df):
-        return (round(df.memory_usage(deep=True).sum() / 1024 ** 2, 2))
-
-    def memory_usage_detailed(df):
-        df.memory_usage(deep=True) / 1024 ** 2
