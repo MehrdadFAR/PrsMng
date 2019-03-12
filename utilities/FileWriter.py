e4a7625309c872190b0409dcf45af0e1a5eaeddc
Thorn <thorn.prive@gmail.com>
2019-03-12 17:45:53 +0100

    FileWriter Updated missing part is filling in naive_Predictor column

--- utilities/FileWriter.py
+++ utilities/FileWriter.py
@@ -9,20 +9,17 @@ class FileWriter:
 
         print("creating column")
         testFile['Naive_Predictor'] = 0
-        for row in testFile.iterrows():
+        for index, row in testFile.iterrows():
             # Cant figure out how to get the naive estimator per event id so for now hardcoded as 1
-            #row['Naive_Predictor'] = 1
-            variable = 1
+            testFile.at[index,'Naive_Predictor'] = 1
 
 
         print("writing to file started.")
-        include_header = True
-        with open(name, 'w') as f:
-            try:
-                testFile.to_csv(f, header=include_header, encoding=anEncoding, index=False)
-                print("testfile is put into csv")
-            except:
-                print("Exception in writing the file.")
+        try:
+            testFile.to_csv(name, index=False, encoding=self.anEncoding)
+            print("testfile is put into csv")
+        except:
+            print("Exception in writing the file.")
 
 
         print("writing to file completed. File name: ", name)
