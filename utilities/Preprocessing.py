41f3f74531a51dcb893f765e177dd63f7d2405c9
Thorn <thorn.prive@gmail.com>
2019-04-04 21:12:41 +0200

    Updated preprocessing

--- utilities/Preprocessing.py
+++ utilities/Preprocessing.py
@@ -8,18 +8,16 @@ class Preprocessing:
 
     def filter(self, df):
         start_date = '01-01-2018'
-        start_date_object = pd.to_datetime(start_date, dayfirst=True, infer_datetime_format=True)
-
         end_date = '28-01-2019'
+        start_date_object = pd.to_datetime(start_date, dayfirst=True, infer_datetime_format=True)
         end_date_object = pd.to_datetime(end_date, dayfirst=True, infer_datetime_format=True)
 
         mask1 = df['event time:timestamp'] <= start_date_object
+        mask2 = df['event time:timestamp'] >= end_date_object
 
-        array_cases = df[mask1]['case concept:name'].unique()
+        array_cases = df[mask1 | mask2]['case concept:name'].unique()
         array_events = list(df[df['case concept:name'].isin(array_cases)].index)
         new_df = df.drop(array_events, axis=0)
 
-        new_df2 = new_df[new_df['event time:timestamp'] < end_date]
-
-        return new_df2
+        return new_df
 
