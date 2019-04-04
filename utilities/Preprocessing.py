8b46a59665a49eaa1d16d76b81265144f083bccc
MehrdadFAR <mh.farsadyar@gmail.com>
2019-04-04 20:37:38 +0200

    small issue with bollean mask solved.
    small issue with bollean mask solved.

--- utilities/Preprocessing.py
+++ utilities/Preprocessing.py
@@ -14,13 +14,12 @@ class Preprocessing:
         end_date_object = pd.to_datetime(end_date, dayfirst=True, infer_datetime_format=True)
 
         mask1 = df['event time:timestamp'] <= start_date_object
-        mask2 = df['event time:timestamp'] < end_date
-
 
         array_cases = df[mask1]['case concept:name'].unique()
         array_events = list(df[df['case concept:name'].isin(array_cases)].index)
         new_df = df.drop(array_events, axis=0)
 
-        new_df = new_df[mask2]
+        new_df2 = new_df[new_df['event time:timestamp'] < end_date]
 
-        return new_df
+        return new_df2
+
