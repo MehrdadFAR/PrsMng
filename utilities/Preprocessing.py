8c4b9fcb6e2d1a9d1fc93c86325a052463af83b9
MehrdadFAR <mh.farsadyar@gmail.com>
2019-04-04 20:17:39 +0200

    The issue with date 2020 solved.
    The issue with date 2020 solved. Added a mask to filter data.

--- utilities/Preprocessing.py
+++ utilities/Preprocessing.py
@@ -10,10 +10,17 @@ class Preprocessing:
         start_date = '01-01-2018'
         start_date_object = pd.to_datetime(start_date, dayfirst=True, infer_datetime_format=True)
 
+        end_date = '28-01-2019'
+        end_date_object = pd.to_datetime(end_date, dayfirst=True, infer_datetime_format=True)
+
         mask1 = df['event time:timestamp'] <= start_date_object
+        mask2 = df['event time:timestamp'] < end_date
+
 
         array_cases = df[mask1]['case concept:name'].unique()
         array_events = list(df[df['case concept:name'].isin(array_cases)].index)
         new_df = df.drop(array_events, axis=0)
+
+        new_df = new_df[mask2]
 
         return new_df
